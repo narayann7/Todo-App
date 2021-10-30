20dc61e97a6f407f5f1a4d1309ac3048a7e56a02
narayann7 <laxminarayanreddy432@gmail.com>
2021-10-30 19:52:13 +0700

    added top part - add note

---
+++ lib/all_constant.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+Color white = const Color(0xfffbfbfb);
+Color background = const Color(0xffebebeb);
+Color black = const Color(0xff000000);
+Color grey = const Color(0xff3d3d3d);
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:todoapp/all_constant.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key? key}) : super(key: key);
@@ -10,8 +12,87 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.white,
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: background,
+      body: Column(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height * 0.30,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(color: background),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  "To Do App",
+                  style: GoogleFonts.getFont('Sen',
+                      fontSize: 45, color: Colors.black),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.035,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: MediaQuery.of(context).size.height * 0.053,
+                      width: MediaQuery.of(context).size.width * 0.70,
+                      child: const TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                        ),
+                      ),
+                      decoration: BoxDecoration(
+                          color: white,
+                          borderRadius: BorderRadius.circular(12),
+                          boxShadow: const [
+                            BoxShadow(
+                                color: Colors.grey,
+                                blurRadius: 4.0,
+                                spreadRadius: 1.5,
+                                offset: Offset(1, 5)),
+                          ]),
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.035,
+                    ),
+                    Container(
+                      height: MediaQuery.of(context).size.height * 0.053,
+                      width: MediaQuery.of(context).size.width * 0.11,
+                      child: Icon(
+                        Icons.add,
+                        color: background,
+                      ),
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: black,
+                          boxShadow: const [
+                            BoxShadow(
+                                color: Colors.grey,
+                                blurRadius: 4.0,
+                                spreadRadius: 1,
+                                offset: Offset(1, 5)),
+                          ]),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                )
+              ],
+            ),
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height * 0.70,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(color: background),
+            child: SingleChildScrollView(
+              child: Column(),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
